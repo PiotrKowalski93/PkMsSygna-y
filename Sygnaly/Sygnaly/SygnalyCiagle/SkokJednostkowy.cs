a5e2092ec051c236b33c4c42585f4cbdd6f63471
Piotr Kowalski <piotr.kowalski@ts.fujitsu.com>
2016-10-26 22:07:46 +0200

    Dodatkowe klasy

---
+++ Sygnaly/Sygnaly/SygnalyCiagle/SkokJednostkowy.cs
@@ -0,0 +1,35 @@
+﻿using Sygnaly.Sygaly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sygnaly.SygnalyCiagle
+{
+    class SkokJednostkowy : SygnalCiagly
+    {
+        public double ts = 5;
+
+        public SkokJednostkowy(double A, double T, double t1, double d)
+        {
+            this.A = A;
+            this.T = T;
+            this.t1 = t1;
+            this.d = d;
+
+            for (int i = 0; i < y.Count; i++)
+            {
+                y[i] = Licz(x[i]);
+            }
+        }
+
+        private double Licz(double x)
+        {
+            if (x < ts)
+                return 0;
+            else
+                return A;
+        }
+    }
+}
---
+++ Sygnaly/Sygnaly/SygnalyCiagle/SygnalProstokatny.cs
@@ -0,0 +1,40 @@
+﻿using Sygnaly.Sygaly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sygnaly.SygnalyCiagle
+{
+    class SygnalProstokatny : SygnalCiagly
+    {
+        private double k = 0.5;
+
+        public SygnalProstokatny(double A, double T, double t1, double d)
+        {
+            this.A = A;
+            this.T = T;
+            this.t1 = t1;
+            this.d = d;
+
+            for (int i = 0; i < y.Count; i++)
+            {
+                y[i] = Licz(x[i]);
+            }
+        }
+
+        private double Licz(double x)
+        {
+            double p = x % T;
+            if (p <= k * T)
+            {
+                return A;
+            }
+            else
+            {
+                return 0.0;
+            }
+        }
+    }
+}
