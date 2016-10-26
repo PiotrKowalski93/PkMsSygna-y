4cd988fee4da616f1580e638d5193eabc3c3d3ff
Piotr Kowalski <piotr.kowalski@ts.fujitsu.com>
2016-10-26 21:48:29 +0200

    Podstawowe klasy

---
+++ Sygnaly/Sygnaly/Sygaly/Sygnal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sygnaly.Sygaly
+{
+    class Sygnal
+    {
+        // Wartosci Opisujace sygnal
+        public double A = 1;       // <-- Amplituda
+        public double T = 2;       // <-- Okres
+        public double t1 = 0;      // <-- Czas Poczatkowy
+        public double d = 10;      // <-- Czas Trwania
+        public double kw;          // <-- Współczynnik wypełnienia (tylko dla prost. i trój.)
+        public int n;              // <-- Ilość próbek sygnału.
+
+        public List<double> y;
+        public List<double> x;
+
+        // Wartości obliczeniowe sygnału.
+        public double srednia;
+        public double sredniaBezwzgledna;
+        public double sredniaMoc;
+        public double wariancja;
+        public double wartoscSkuteczna;
+
+        public Sygnal()
+        {
+            n = 1000;
+            x = new List<double>();
+            y = new List<double>();
+
+            double j = 0;
+
+            for (int i = 0; i < x.Count; i++)
+            {
+                x.Add(j);
+                j = j + (d / 1000);
+            }
+        }
+    }
+}
---
+++ Sygnaly/Sygnaly/SygnalyCiagle/SzumRozdladJednostajny.cs
@@ -0,0 +1,27 @@
+﻿using Sygnaly.Sygaly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sygnaly.SygnalyCiagle
+{
+    class SzumRozdladJednostajny : SygnalCiagly
+    {
+        private Random random = new Random();
+
+        SzumRozdladJednostajny(double A, double t1, double d)
+        {
+            this.A = A;
+            this.t1 = t1;
+            this.d = d;
+            
+            for(int i = 0; i <= y.Count; i++)
+            {
+                y[i] = random.NextDouble() * A;
+            }
+        }
+
+    }
+}
